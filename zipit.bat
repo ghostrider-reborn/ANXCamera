5b8c0de2b4962dc91626f961f8adb9537f5abd9f
XEonAX <sv.xeon@gmail.com>
2019-08-08 21:40:02 +0530

    Fix davinci libs
    (cherry picked from commit 61bb40917393a16c09bcb4143fefb2e01e629432)
    
    # Conflicts:
    #	VERSION
    #	src/ANXCamera/AndroidManifest.xml
    #	src/ANXCameraUnity/module.prop

--- zipit.bat
+++ zipit.bat
@@ -18,5 +18,12 @@ REM copy .\out\Dyneteve48MPFix.zip /B .\out\Dyneteve48MPFix_%anxversion%.zip
 REM "C:\Program Files\7-Zip\7z.exe" a -tzip .\out\KubilWhyredyFix.zip .\src\KubilWhyredyFix\*
 REM copy .\out\KubilWhyredyFix.zip /B .\out\KubilWhyredyFix_%anxversion%.zip
 
-adb push .\out\ANXCameraUnity.zip  /sdcard/zips
+REM adb push .\out\ANXCameraUnity.zip  /sdcard/zips
+
+REM del .\out\ANXCamFix.zip
+REM del .\out\ANXCamFix_*.zip
+REM "C:\Program Files\7-Zip\7z.exe" a -tzip .\out\ANXCamFix.zip .\src\ANXCamFix\*
+REM copy .\out\ANXCamFix.zip /B .\out\ANXCamFix_%anxversion%.zip
+
+REM adb push .\out\ANXCamFix.zip  /sdcard/zips
 
