56c7c07aec12491b868e26471b23677e83a7d741
XEonAX <sv.xeon@gmail.com>
2019-08-13 23:47:50 +0530

    try chiron, jason, mido, sagit, sakura, vince and wayne
    (cherry picked from commit 53321da1dbc7678cf21c94581e4d76ede2e95824)
    
    # Conflicts:
    #	src/ANXCamera/sources/com/android/camera/R.java

--- zipit.bat
+++ zipit.bat
@@ -7,7 +7,7 @@ del .\out\ANXCameraUnity_*.zip
 
 "C:\Program Files\7-Zip\7z.exe" a -tzip .\out\ANXCameraUnity.zip .\src\ANXCameraUnity\*
 copy .\out\ANXCameraUnity.zip /B .\out\ANXCameraUnity_%anxversion%.zip
-
+adb push .\out\ANXCameraUnity.zip  /sdcard/zips
 
 REM "C:\Program Files\7-Zip\7z.exe" a -tzip .\out\Arnob48MPFix.zip .\src\Arnob48MPFix\*
 REM copy .\out\Arnob48MPFix.zip /B .\out\Arnob48MPFix_%anxversion%.zip
@@ -18,12 +18,13 @@ REM copy .\out\Dyneteve48MPFix.zip /B .\out\Dyneteve48MPFix_%anxversion%.zip
 REM "C:\Program Files\7-Zip\7z.exe" a -tzip .\out\KubilWhyredyFix.zip .\src\KubilWhyredyFix\*
 REM copy .\out\KubilWhyredyFix.zip /B .\out\KubilWhyredyFix_%anxversion%.zip
 
-REM adb push .\out\ANXCameraUnity.zip  /sdcard/zips
 
 REM del .\out\ANXCamFix.zip
 REM del .\out\ANXCamFix_*.zip
-REM "C:\Program Files\7-Zip\7z.exe" a -tzip .\out\ANXCamFix.zip .\src\ANXCamFix\*
-REM copy .\out\ANXCamFix.zip /B .\out\ANXCamFix_%anxversion%.zip
+REM del .\out\ANXCamCustVendFix.zip
+REM del .\out\ANXCamCustVendFix_*.zip
+REM "C:\Program Files\7-Zip\7z.exe" a -tzip .\out\ANXCamCustVendFix.zip .\src\ANXCamFix\*
+REM copy .\out\ANXCamCustVendFix.zip /B .\out\ANXCamCustVendFix_%anxversion%.zip
 
-REM adb push .\out\ANXCamFix.zip  /sdcard/zips
+REM adb push .\out\ANXCamCustVendFix.zip  /sdcard/zips
 
