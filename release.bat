226e429324be4f5343d3a115d99668aea53ac735
XEonAX <sv.xeon@gmail.com>
2019-09-04 19:40:05 +0530

    LavendyFix
    (cherry picked from commit cd23fc2c07865d38f6291b6eb55c2f6ecb04aabf)
    
    # Conflicts:
    #	release.bat

--- release.bat
+++ release.bat
@@ -2,7 +2,7 @@ for /f "delims== tokens=1,2" %%G in (VERSION) do set anx%%G=%%H
 echo %anxversion%
 git tag %anxversioncode%.oos_singularity
 git push origin %anxversioncode%.oos_singularity
-..\ANXMiuiPortTools\hub.exe release create -a .\out\ANXCameraUnity_%anxversion%.zip -m oos_singularity.%anxversion% %anxversioncode%.oos_singularity
+..\ANXMiuiPortTools\hub.exe release create -d -a .\out\ANXCameraUnity_%anxversion%.zip -m oos_singularity.%anxversion% %anxversioncode%.oos_singularity
 REM ..\ANXMiuiPortTools\hub.exe release edit -a .\out\ANXCameraUnity_%anxversion%.zip -m oos_singularity.%anxversion% %anxversioncode%.oos_singularity
 REM ..\ANXMiuiPortTools\hub.exe release edit -a .\out\KubilWhyredyFix_%anxversion%.zip -m oos_singularity.%anxversion% %anxversioncode%.oos_singularity
 REM ..\ANXMiuiPortTools\hub.exe release edit -a .\out\Dyneteve48MPFix_%anxversion%.zip -m oos_singularity.%anxversion% %anxversioncode%.oos_singularity
