649760ebb8865a28da203cc02b11c4ca132e9b1f
Nico Kümmel <nico.kuemmel@gmail.com>
2019-11-03 16:32:52 +0100

    Fixed derp in sign

--- sign.sh
+++ sign.sh
@@ -6,4 +6,4 @@ BUILDTOOLSPATH=/opt/android-sdk/build-tools/29.0.2
 cp ./out/ANXCamera-unsigned.apk ./out/ANXCamera-to-be-signed.apk
 $BUILDTOOLSPATH/apksigner sign --key ../ANXMiuiPortTools/testkey.pk8 --cert ../ANXMiuiPortTools/testkey.x509.pem ./out/ANXCamera-to-be-signed.apk
 mv ./out/ANXCamera-to-be-signed.apk ./out/ANXCamera-Unaligned.apk
-$BUILDTOOLSPATH/zipalign -f 4  ./out/ANXGallery-Unaligned.apk ./out/ANXCamera.apk
+$BUILDTOOLSPATH/zipalign -f 4  ./out/ANXCamera-Unaligned.apk ./out/ANXCamera.apk
