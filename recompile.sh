42999d8f9034af25ea4987c66a3fbebc9a81ab12
Adithya R <radithya2002@gmail.com>
2020-05-12 23:14:14 +0530

    ANXCamera: Use bundled apktool for compilation
    * Apktool version 2.4.0-dirty that currently comes with ubuntu
      installations throws the following error on compilation:
    
    $ bash recompile.sh
    I: Using Apktool 2.4.0-dirty
    I: Checking whether sources has changed...
    I: Checking whether sources has changed...
    I: Smaling smali_classes5 folder into classes5.dex...
    Exception in thread "main" java.lang.NoSuchMethodError: java.nio.ByteBuffer.clear()Ljava/nio/ByteBuffer;
            at org.jf.dexlib2.writer.DexWriter.writeAnnotationDirectories(DexWriter.java:790)
            at org.jf.dexlib2.writer.DexWriter.writeTo(DexWriter.java:340)
            at org.jf.dexlib2.writer.DexWriter.writeTo(DexWriter.java:297)
            at brut.androlib.src.SmaliBuilder.build(SmaliBuilder.java:61)
            at brut.androlib.src.SmaliBuilder.build(SmaliBuilder.java:36)
            at brut.androlib.Androlib.buildSourcesSmali(Androlib.java:419)
            at brut.androlib.Androlib.buildNonDefaultSources(Androlib.java:365)
            at brut.androlib.Androlib.build(Androlib.java:303)
            at brut.androlib.Androlib.build(Androlib.java:269)
            at brut.apktool.Main.cmdBuild(Main.java:247)
            at brut.apktool.Main.main(Main.java:79)

--- recompile.sh
+++ recompile.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-apktool b -p ../ANXMiuiPortTools/MiuiFrameworks -o ./out/ANXCamera-Unsigned.apk ./src/ANXCamera
+java -jar ../ANXMiuiPortTools/apktool.jar b -p ../ANXMiuiPortTools/MiuiFrameworks -o ./out/ANXCamera-Unsigned.apk ./src/ANXCamera
